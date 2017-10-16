0f4361ecbe282159f198938bb1f95430763ca3c1
Miguh Ruiz <miguhruiz@gmail.com>
2017-10-16 18:59:09 +0200

    [Add] Posts to the Main Page

---
+++ components/Posts.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import moment from 'moment'
+
+class Posts extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            posts: []
+        }
+
+    }
+    componentDidMount() {
+        fetch('https://owcrud-api.now.sh/api/posts')
+            .catch(err => console.error(err))
+            .then(res => res.json())
+            .then(posts => this.setState({ posts }))
+    }
+    render() {
+        if(this.state.posts.length > 0) {
+            return(
+                <div className="Posts">
+                    {
+                        this.state.posts.map((post) => (
+                            <div className="Posts-Item">
+                                <div className="PhotoSegment">
+                                    <img src={post.image} alt={post.title}/>
+                                </div>
+                                <div className="DetailsSegment">
+                                    <h2 className="Post-Title">{post.title}</h2>
+                                    <h3 className="Post-Date">{moment(post.releaseDate).fromNow()}</h3>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            )
+        } else {
+            return(
+                <h3>Cargando...</h3>
+            )
+        }
+    }
+}
+
+export default Posts
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import Posts from '../components/Posts'
+
 function FirstPage() {
     return(
         <div className="App">
-            <h1>Hello World</h1>
+            <Posts />
         </div>
     )
 }
