07bf458916c5d1587d79ac2e8fa56f0d05da7186
Miguh Ruiz <miguhruiz@gmail.com>
2017-10-16 19:59:03 +0200

    [Add] Styles to Posts

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Head from 'next/head'
 
 import Posts from '../components/Posts'
 
 function FirstPage() {
     return(
         <div className="App">
+            <Head>
+                <link rel="stylesheet" href="/static/app.css"/>
+            </Head>
             <Posts />
         </div>
     )
