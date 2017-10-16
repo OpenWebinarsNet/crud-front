b460137ff3dbc4f0d628fe2e1bdb7876e8952ba4
Miguh Ruiz <miguhruiz@gmail.com>
2017-10-16 18:23:45 +0200

    [Init] Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FirstPage() {
+    return(
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default FirstPage
