b2b4c65ca40a5323a93e73a43d8d44b6665269e4
Miguh Ruiz <miguhruiz@gmail.com>
2017-10-18 17:27:00 +0200

    [Add] React Router

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
+import { HashRouter, Route, history } from 'react-router-dom'
 
 import Posts from '../components/Posts'
 
@@ -10,7 +11,11 @@ function FirstPage() {
                 <link rel="stylesheet" href="/static/app.css"/>
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
             </Head>
-            <Posts />
+            <HashRouter history={history}>
+                <div id="Routes">
+                    <Route path='/' component={Posts}/>
+                </div>
+            </HashRouter>
         </div>
     )
 }
